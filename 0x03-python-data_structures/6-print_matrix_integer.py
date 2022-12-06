420ddc262b4c9ecac844d4168c927a8e78d9fc4b
toydaboss <toyeseaderibigbe@gmail.com>
2022-12-05 21:24:21 -0800

    matric fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
     for row in matrix:
-        for i in row:
-            if i != row[-1]:
-                print("{:d}".format(i), end="")
-            else:
-                print("{:d}".format(i), end="")
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
