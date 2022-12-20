525193bb175f9c8c83feb1bc46165e8a7008b3f5
toydaboss <toyeseaderibigbe@gmail.com>
2022-12-20 05:22:44 -0800

    python exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+	while isinstance(value, int):
+		try:
+			print("{:d}".format(value))
+			return True
+		except (ValueError, TypeError):
+			break
+	return False
